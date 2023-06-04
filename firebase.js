b38550075800e2a6f458d60ab14503fc89bf6dca
DeveKoke <jorge.be86@gmail.com>
2023-06-04 22:47:22 +0200

    CSS/nav_menu

---
+++ firebase.js
@@ -0,0 +1,129 @@
+// * Firebase Configuración inicial del proyecto ----------------------------------------------
+const firebaseConfig = {
+    apiKey: "AIzaSyDc5VAgj7ApSveTTNZJjx-7zSWh4bk9ayk",
+    authDomain: "f1-fantasy-1f8d9.firebaseapp.com",
+    projectId: "f1-fantasy-1f8d9",
+    storageBucket: "f1-fantasy-1f8d9.appspot.com",
+    messagingSenderId: "452499597572",
+    appId: "1:452499597572:web:89e770086a89aca02e8851"
+};
+
+const firebaseInitialize = firebase.initializeApp(firebaseConfig);// Inicializaar app Firebase
+const db = firebase.firestore();// db representa mi BBDD //inicia Firestore
+const auth = firebase.auth();   //Initialize FireAuth
+const user = auth.currentUser;  //Initialize FireAuth
+
+
+// const app = initializeApp(firebaseConfig);   // Initialize Firebase
+// const db = getFirestore(app);   //Initialize DDBB
+//*----------------------------------------------------------------------------------------------
+
+let signUpForm = document.getElementById("signUp_form");
+let logInForm = document.getElementById("logIn_form");
+const emailRegex = /^[\w\._]+@([\w-]+\.)+[\w-]{2,4}$/;
+const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d).{6,}$/;
+
+//* CAMBIAR MENÚ LOGIN => SIGNUP
+let signUpButton = document.getElementById('signupFreeButton');
+signUpButton.addEventListener('click', (event)=>{
+    event.preventDefault();
+    let logInForm = document.getElementById('logIn_form');
+    let signUpForm = document.getElementById('signUp_form');
+    logInForm.style.display = 'none'
+    signUpForm.style.display = 'flex'
+})
+
+//* CREAR DOCUMENTO DE COLECCION USERS EN FIRESTORE
+const createUser = (user)=>{
+    let userName = document.getElementById('signup-username').value;
+  // event.preventDefault();
+  console.log(user);
+  db.collection("users")
+  .add(user)
+  .then((docRef) => console.log("Document written with ID: ", docRef.id))  //* => meter identificador random = docRef
+  .catch((error) => console.error("Error adding document: ", error));
+
+};
+
+
+//*Sign up function y creación de documento de usuario en FIRESTORE  ----------------
+signUpForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  let userName = document.getElementById('signup-username').value;
+  let userEmail = document.getElementById('signup-email').value;
+  let userPass = document.getElementById('signup-pass').value;
+  let repPass = document.getElementById('signup-reppass').value;
+
+  if (!emailRegex.test(userEmail)) {
+    alert("Invalid email format");
+    return;
+  }
+  if (!passwordRegex.test(userPass)) {
+    alert("Invalid password format. It should have at least 6 characters and include letters and numbers.");
+    return;
+  }
+  if(userPass != repPass){
+      alert("Repeated password did not match with the first one.")
+      return;
+  }
+  try{  //CREATE AUTH USER
+      await auth.createUserWithEmailAndPassword(userEmail, userPass)
+      .then((userCredential) => {
+          console.log('User registered');
+          const user = userCredential.user;
+          signUpForm.reset();
+          // Guarda El usuario en Firestore
+          createUser({                  
+            user_name: userName,
+            email: userEmail,
+            });
+          window.location.href = 'home.html';    //? Cambiar de documento HTML al registrarse.
+      })
+  }catch (error) {
+    console.log(`There has been an error with code: ${error.code}: ${error.message}`)
+  } 
+})
+
+
+//* Log in function -----------------------------
+logInForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  let userNameLog = document.getElementById("login-userEmail").value;
+  let userPassLog = document.getElementById("login-pass").value;
+  if (!emailRegex.test(userEmail)) {
+    alert("Invalid email format");
+    return;
+  }
+  if (!passwordRegex.test(userPass)) {
+    alert("Invalid password format. It should have at least 6 characters and include letters and numbers.");
+    return;
+  }
+  auth.signInWithEmailAndPassword(userNameLog, userPassLog)
+  .then((userCredential) => {
+    console.log('User authenticated')
+    const user = userCredential.user;
+    logInForm.reset();
+  })
+  .then(() => {
+      window.location.href = 'home.html';    //? Cambiar de documento HTML al registrarse.
+      console.log(`Hello, ${userNameLog}`);
+    })
+  .catch((error) => {
+    alert("Invalid user or password. Try again");
+    console.log('Invalid user or password');
+    console.log(`There has been an error with code: ${error.code}: ${error.message}`)
+  });
+
+})
+
+
+//* LOGOUT function.
+const logOutButton = document.getElementById("logOutButton");
+logOutButton.addEventListener('click', function (){
+    auth.signOut().then(() => {
+      console.log('Logout user');
+
+    }).catch((error) => {
+      console.log('Error: ', error)
+    });
+})
--- scripts.js
+++ scripts.js
@@ -1,16 +1,4 @@
-// * Firebase Configuración inicial del proyecto ----------------------------------------------
-const firebaseConfig = {
-    apiKey: "AIzaSyDc5VAgj7ApSveTTNZJjx-7zSWh4bk9ayk",
-    authDomain: "f1-fantasy-1f8d9.firebaseapp.com",
-    projectId: "f1-fantasy-1f8d9",
-    storageBucket: "f1-fantasy-1f8d9.appspot.com",
-    messagingSenderId: "452499597572",
-    appId: "1:452499597572:web:89e770086a89aca02e8851"
-};
-
-
-
-//* HAMBURGUER MENU DEL NAVBAR Y SU DESPLIEGUE
+//* HAMBURGUER MENU DEL NAVBAR Y SU DESPLIEGUE   ---------------------------------
 const menu = document.getElementById('menu')
 const menuIcon = document.getElementById('menu_icon')
 const menuUl = document.createElement('ul'); 
@@ -18,138 +6,28 @@ menuUl.setAttribute('class','menu_ul');
 
 menuIcon.addEventListener('click', () => {
   const menuItems = `
-  <li><a href="home.html">TU EQUIPO</a></li>
-  <li><a href="resultados.html">RESULTADOS</a></li>
-  <li><a href="liga.html">LIGA</a></li>
+  <li><a href="home.html">YOUR TEAM</a></li>
+  <li><a href="resultados.html">RESULTS</a></li>
+  <li><a href="liga.html">LEAGUE</a></li>
 `;
-
-  menuUl.innerHTML= menuItems;
-  if (menuUl.style.display === 'none') {
-    menuUl.style.display = 'flex';
-  } else {
-    menuUl.style.display = 'none';
-  }
-  menu.appendChild(menuUl)
-
+if (menuUl.style.display === 'none') {
+  setTimeout(function() {
+    menu.style.opacity = '1';
+  }, 100);
+  menuUl.style.display = 'flex';
+  menu.style.borderTop = 'solid';
+  menu.style.borderTopColor = 'rgb(203, 25, 25)';
+} else {
+  // setTimeout(function() {
+  //   menu.style.opacity = '1';
+  // }, 2000);
+  menuUl.style.display = 'none';
+  menu.style.borderTop = 'none';
+}
+menu.appendChild(menuUl);
+menuUl.innerHTML= menuItems;
 });
 
-
-
-
-
-
-
-
-
-
-// const firebaseInitialize = firebase.initializeApp(firebaseConfig);// Inicializaar app Firebase
-// const db = firebase.firestore();// db representa mi BBDD //inicia Firestore
-// const auth = firebase.auth();   //Initialize FireAuth
-// const user = auth.currentUser;  //Initialize FireAuth
-
-
-// const app = initializeApp(firebaseConfig);   // Initialize Firebase
-// const db = getFirestore(app);   //Initialize DDBB
-//*----------------------------------------------------------------------------------------------
-
-let signUpForm = document.getElementById("signUp_form");
-let logInForm = document.getElementById("logIn_form");
-let logOutButton = document.getElementById("logOut_button");
-const emailRegex = /^[\w\._]+@([\w-]+\.)+[\w-]{2,4}$/;
-const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d).{6,}$/;
-
-//* CAMBIAR MENÚ LOGIN => SIGNUP
-let signUpButton = document.getElementById('signupFreeButton');
-signUpButton.addEventListener('click', (event)=>{
-    event.preventDefault();
-    let logInForm = document.getElementById('logIn_form');
-    let signUpForm = document.getElementById('signUp_form');
-    logInForm.style.display = 'none'
-    signUpForm.style.display = 'flex'
-})
-
-//* CREAR DOCUMENTO DE USUARIO EN FIRESTORE
-const createUser = (user)=>{
-  // event.preventDefault();
-  console.log(user);
-  db.collection("users")
-  .add(user)
-  // .then((userName) => console.log("Document written with ID: ", userName.id))
-  .then((docRef) => console.log("Document written with ID: ", docRef.id))  //* => meter identificador random = docRef
-  .catch((error) => console.error("Error adding document: ", error));
-
-};
-
-//*Sign up function y creación de documento de usuario en FIRESTORE  ----------------
-signUpForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    let userName = document.getElementById('signup-username').value;
-    let userEmail = document.getElementById('signup-email').value;
-    let userPass = document.getElementById('signup-pass').value;
-    let repPass = document.getElementById('signup-reppass').value;
-
-    if (!emailRegex.test(userEmail)) {
-      alert("Invalid email format");
-      return;
-    }
-    if (!passwordRegex.test(userPass)) {
-      alert("Invalid password format. It should have at least 6 characters and include letters and numbers.");
-      return;
-    }
-    if(userPass != repPass){
-        alert("Repeated password did not match with the first one.")
-        return;
-    }
-    try{  //CREATE AUTH USER
-        await auth.createUserWithEmailAndPassword(userEmail, userPass)
-        .then((userCredential) => {
-            console.log('User registered');
-            const user = userCredential.user;
-            signUpForm.reset();
-            window.location.href = 'home.html';    //? Cambiar de documento HTML al registrarse.
-
-            // Guarda El usuario en Firestore
-            createUser({                  
-              user_name: userName,
-              email: userEmail,
-              });
-        })
-    }catch (error) {
-      console.log(`There has been an error with code: ${error.code}: ${error.message}`)
-    } 
-})
-
-
-//* Log in function -----------------------------
-logInForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    let userNameLog = document.getElementById("login-userEmail").value;
-    let userPassLog = document.getElementById("login-pass").value;
-    if (!emailRegex.test(userEmail)) {
-      alert("Invalid email format");
-      return;
-    }
-    if (!passwordRegex.test(userPass)) {
-      alert("Invalid password format. It should have at least 6 characters and include letters and numbers.");
-      return;
-    }
-    auth.signInWithEmailAndPassword(userNameLog, userPassLog)
-    .then((userCredential) => {
-      console.log('User authenticated')
-      const user = userCredential.user;
-      logInForm.reset();
-    })
-    .then(() => {
-        window.location.href = 'home.html';    //? Cambiar de documento HTML al registrarse.
-        console.log(`Hello, ${userNameLog}`);
-      })
-    .catch((error) => {
-      alert("Invalid user or password. Try again");
-      console.log('Invalid user or password');
-      console.log(`There has been an error with code: ${error.code}: ${error.message}`)
-    });
-
-})
 
 
 //* Crear identificador usuario en nav
@@ -160,5 +38,83 @@ logInForm.addEventListener("submit", async (e) => {
 //                   </div>`;
 //   header.innerHTML += user_idBar;
 // }
+
+
+
+// * FUNCIÓN PARA ACCEDER A API Y PINTAR TARGETA PILOTO    -------------------------
+var arrIndex = 0;
+const driversList = ["vers","per","lec","sai","lew","russ","alo","stro","oco","gas","nor","pia","bot","zho","tsu","vri","hul","mag","alb","sar"];
+
+async function getDriverInfoCard(driverId) {
+  try{
+      await fetch(`https://v1.formula-1.api-sports.io/drivers?search=${driverId}`, {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "api-formula-1.p.rapidapi.com",
+          "x-rapidapi-key": "8cd55f4fc36d00125765355eaf63e045"
+        }
+      })
+      .then(response => {
+        let data = response.json();
+        let driverName = data.response.name;
+        let driverImg = data.response.image;
+        let driverNumber = data.response.number;
+        let driverCountry= data.response.country.name;
+        let driverChamps = data.response.world_championships;
+        let driverRaces = data.response.grands_prix_entered;
+        let driverVic = data.response.highest_race_finish.number;
+        let driverPodiums = data.response.podiums;
+        let driverPoints = data.response.career_points;
+        cardContainer = document.querySelector('#carousel-wrapper');
+        carouselCards = document.createElement('article');
+        let driverCard = ` <h2>${driverName}</h2>
+        <img src="${driverImg}" alt="piloto ${driverName}">
+        <p>Number: ${driverNumber}</p>
+        <p>Country: ${driverCountry}</p>
+        <p>World Championships: ${driverChamps}</p>
+        <p>Races: ${driverRaces}</p>
+        <p>Victories: ${driverVic}</p>
+        <p>Podiums: ${driverPodiums}</p>
+        <p>Carrer Points: ${driverPoints}</p>`
+        carouselCards.innerHTML = driverCard;
+        cardContainer.appendChild(carouselCards);
+        
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }catch (error) {
+    console.error(error);
+  }
+}
+
+// * FUNCIÓN PARA BOTONES R&L  DEL CARRUSEL   --------------------------------------
+const btnLeft = document.getElementById("btn-left");
+const btnRight = document.getElementById("btn-right");
+
+function getDriverApiRight() {
+  arrIndex ++;
+  if (arrIndex > 19) {
+    arrIndex = 0; 
+  }
+  let driverName = driversList[arrIndex]
+  console.log(driverName);
+  getDriverInfoCard(driverName)    
+}
+function getDriverApiLeft() {
+  arrIndex --;
+  if (arrIndex < 0) {
+    arrIndex = 19;
+  }
+  let driverName = driversList[arrIndex]
+  console.log(driverName);
+  getDriverInfoCard(driverName) 
+}
+
+btnRight.addEventListener("click",getDriverApiRight);
+btnLeft.addEventListener("click", getDriverApiLeft);
+
+
+
 
 
